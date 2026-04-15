5fc519e91c1ffc870fc631a7e650e00454f062fe
Mohsen <bdra77367@gmail.com>
2026-04-15 16:49:25 +0530

    Update main.jsx

--- frontend/main.jsx
+++ frontend/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Dwork from './Dwork.jsx'
+import Dwork from './Dwork.jsx' // Make sure this matches your filename exactly
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
